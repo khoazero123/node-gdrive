9500835042dafbced3af3a93a44b7854d945e81d
KhoaZero123 <khoazero123@gmail.com>
2019-03-22 13:25:33 +0700

    Update

--- test/upload.js
+++ test/upload.js
@@ -1,11 +1,17 @@
 
-const { Upload, client } = require("../lib/upload");
+const { Upload} = require("../");
 
 
 const filePath = process.argv[2] || 'README.md';
 
 var upload = new Upload(filePath);
-// upload.then(file => console.log).catch(console.error);
 upload.on('*', (event, data) => {
-  console.log(event, data)
+  console.log(event, data);
 });
+
+/* var upload = new Upload();
+upload.upload(filePath).then((file) => {
+  console.log('file', file);
+}).catch((err) => {
+  console.error('err', err);}
+); */
